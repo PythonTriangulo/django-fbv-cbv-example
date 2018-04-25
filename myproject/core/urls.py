a22ebc54613f14b239df343206fe181da6b69746
rg3915 <rg3915@yahoo.com.br>
2018-04-25 18:49:58 -0300

    CBV

--- myproject/core/urls.py
+++ myproject/core/urls.py
@@ -4,6 +4,11 @@ from myproject.core import views as v
 app_name = 'core'
 
 urlpatterns = [
-    path('', v.index, name='index'),
+    path('', v.Index.as_view(), name='index'),
+    path('home/', v.home, name='home'),
+    path('book/', v.BookList.as_view(), name='book_list'),
     path('book/<int:pk>/', v.book_detail, name='book_detail'),
+    path('book/book/<int:pk>/', v.BookDetail.as_view(), name='book_detail'),
+    path('book/add/', v.book_add, name='book_add'),
+    path('book/book/add/', v.BookCreate.as_view(), name='book_add'),
 ]
--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 from .models import Book
+from .forms import BookForm
+
+
+def home(request):
+    return HttpResponse('<h1>Bookstore</h1>')
 
 
 def index(request):
@@ -9,8 +17,58 @@ def index(request):
     return render(request, 'index.html', context)
 
 
+class Index(View):
+
+    def get(self, request):
+        context = {}
+        book_list = Book.objects.all()
+        context['book_list'] = book_list
+        return render(request, 'index.html', context)
+
+
+class BookList(TemplateView):
+    model = Book
+    template_name = 'core/book_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        book_list = Book.objects.all()
+        context['book_list'] = book_list
+        return context
+
+
+class BookList(ListView):
+    model = Book
+    template_name = 'core/book_list.html'
+    context_object_name = 'book_list'
+
+
 def book_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
     context = {}
     context['book'] = book
     return render(request, 'core/book_detail.html', context)
+
+
+class BookDetail(DetailView):
+    model = Book
+    template_name = 'core/book_detail.html'
+    context_object_name = 'book'
+
+
+def book_add(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('core:book_list')
+    else:
+        form = BookForm()
+    context = {'form': form}
+    return render(request, 'core/book_add.html', context)
+
+
+class BookCreate(CreateView):
+    model = Book
+    template_name = 'core/book_add.html'
+    form_class = BookForm
